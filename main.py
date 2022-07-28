023431cc8a66ca4418d5ab87e96bc183b0a6a284
MrAuk <mr.aukenov2002@gmail.com>
2022-07-28 18:45:24 +0600

    changed post link

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from services import get_person_sex, get_person_age
 app = FastAPI()
 
 
-@app.post("/check-sex")
-async def check_sex(person_data: Dict[str, str]) -> Dict[str, bool]:
-    sex: bool = get_person_sex(person_data["iin"])
+@app.post("/{iin}/check-sex/")
+async def check_sex(iin: str) -> Dict[str, bool]:
+    sex: bool = get_person_sex(iin)
     return {"sex": sex}
 
 
-@app.post("/check-birthdate")
-async def check_gender(person_data: Dict[str, str]) -> Dict[str, int]:
+@app.post("/{iin}/check-birthdate/")
+async def check_gender(iin: str) -> Dict[str, int]:
     try:
-        age: int = get_person_age(person_data["iin"])
+        age: int = get_person_age(iin)
         return {"age": age}
     except ValueError:
         print("inappropriate value for iin")

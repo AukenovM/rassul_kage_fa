cc19a21656aae9bdf589277302cdca78d8f676e6
MrAuk <mr.aukenov2002@gmail.com>
2022-07-29 11:59:57 +0600

    change of static date to actual date from datetime

--- services.py
+++ services.py
@@ -1,3 +1,8 @@
+from datetime import date
+today = date.today()
+d1 = today.strftime("%d/%m/%Y")
+
+
 def get_person_sex(person_data: str) -> bool:
     try:
         if int(person_data[6]) % 2 != 0:
@@ -10,16 +15,16 @@ def get_person_sex(person_data: str) -> bool:
 
 def get_person_century(person_age: str):
     if person_age[6] in '56':
-        return 2022 - int('20' + person_age[:2])
+        return int(d1[6:10]) - int('20' + person_age[:2])
     else:
-        return 2022 - int('19' + person_age[:2])
+        return int(d1[6:10]) - int('19' + person_age[:2])
 
 
 def get_person_age(person_age: str) -> int:
     try:
         age: int = get_person_century(person_age)
-        month: int = 7 - int(person_age[2:4])
-        day: int = 28 - int(person_age[4:6])
+        month: int = int(d1[3:5]) - int(person_age[2:4])
+        day: int = int(d1[:2]) - int(person_age[4:6])
 
         if (month > 0 or day >= 0) or (month == 0 and day >= 0):
             return age
